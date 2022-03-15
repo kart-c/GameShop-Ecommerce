71d50b327a1a6a1f04b86566b942981e5a942f87
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-15 17:19:16 +0530

    docs: add image assets

---
+++ src/Assets/images.js
@@ -0,0 +1,19 @@
+import HeroImg1 from './hero-image-1.webp';
+import HeroImg2 from './hero-image-2.jpg';
+import Category1_1 from './category-img-1__1.webp';
+import Category1_2 from './category-img-1__2.jpg';
+import Category2_1 from './category-img-2__1.webp';
+import Category2_2 from './category-img-2__2.jpg';
+import Category3_1 from './category-img-3__1.webp';
+import Category3_2 from './category-img-3__2.jpg';
+
+export {
+	HeroImg1,
+	HeroImg2,
+	Category1_1,
+	Category1_2,
+	Category2_1,
+	Category2_2,
+	Category3_1,
+	Category3_2,
+};
