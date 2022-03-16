f73a247322997a76f93b048a203d6f850b2da2c9
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-16 16:44:10 +0530

    feat: add carousel functionality

--- src/Pages/Home/Components/Carousel/Carousel.jsx
+++ src/Pages/Home/Components/Carousel/Carousel.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { carouselImgs } from '../../../../Assets/images';
 import styles from './Carousel.module.css';
-import { Carousel1 } from '../../../../Assets/images';
 
 const Carousel = () => {
+	const [currentImg, setCurrentImg] = useState(0);
+
+	const carouselHandler = (btnName) => {
+		if (btnName === 'prev') {
+			currentImg === 0 ? setCurrentImg(carouselImgs.length - 1) : setCurrentImg((prev) => prev - 1);
+		}
+		if (btnName === 'next') {
+			currentImg === carouselImgs.length - 1 ? setCurrentImg(0) : setCurrentImg((prev) => prev + 1);
+		}
+	};
+
 	return (
 		<div className={styles.lpCarouselContainer}>
 			<Link to="/products">
-				<img src={Carousel1} alt="carousel image 1" className="resp-img" />
+				<img src={carouselImgs[currentImg]} alt="carousel image 1" className="resp-img" />
 			</Link>
-			<button className={styles.carouselLeft}>
+			<button className={styles.carouselLeft} onClick={() => carouselHandler('prev')}>
 				<i className="fa-solid fa-circle-chevron-left"></i>
 			</button>
-			<button className={styles.carouselRight}>
+			<button className={styles.carouselRight} onClick={() => carouselHandler('next')}>
 				<i className="fa-solid fa-circle-chevron-right"></i>
 			</button>
 		</div>
