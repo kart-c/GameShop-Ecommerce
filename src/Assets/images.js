f4a4a32764c1ebab52e2f4d530961e8f1a7ce06d
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-17 12:56:36 +0530

    chore: add new images for database and export them

--- src/Assets/images.js
+++ src/Assets/images.js
@@ -1,9 +1,34 @@
-import HeroImg from './horizon.jpg';
+import horizon from './horizon.jpg';
 import RDR2 from './RDR2.jpg';
 import Fifa from './Fifa.jpg';
 import AOE4 from './AOE4.jpg';
 import GOW from './God of war.jpg';
+import Batman from './arkham knight.jpg';
+import EldenRing from './Elden Ring.jpg';
+import valorant from './valorant.jpg';
+import civilization from './civilization.jpg';
+import AOE2 from './AOE2.jpg';
+import NBA from './NBA.jpg';
+import sekiro from './sekiro.jpg';
+import witcher from './witcher.jpg';
+import mario from './mario.jpg';
 
 const carouselImgs = [GOW, RDR2, Fifa];
 
-export { HeroImg, RDR2, Fifa, AOE4, GOW, carouselImgs };
+export {
+	horizon,
+	RDR2,
+	Fifa,
+	AOE4,
+	GOW,
+	carouselImgs,
+	Batman,
+	EldenRing,
+	valorant,
+	civilization,
+	AOE2,
+	NBA,
+	sekiro,
+	witcher,
+	mario,
+};
