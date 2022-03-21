1c0b98df7a79d694b05860750119d7f063e13c74
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-21 23:08:08 +0530

    feat: update reducer
    update reducer function to call increment, decrement and remove from cart functionalityh

--- src/Reducer/Cart Reducer/cart-reducer.js
+++ src/Reducer/Cart Reducer/cart-reducer.js
@@ -2,8 +2,21 @@ export const cartReducer = (state, action) => {
 	const { payload } = action;
 
 	switch (action.type) {
+		case 'INITIAL':
+			return { ...state, cart: payload };
+
 		case 'ADD_TO_CART':
 			return { ...state, cart: payload };
+
+		case 'INCREMENT':
+			return { ...state, cart: payload };
+
+		case 'DECREMENT':
+			return { ...state, cart: payload };
+
+		case 'REMOVE_FROM_CART':
+			return { ...state, cart: payload };
+
 		default:
 			throw new Error('NO CASE FOUND IN CART REDUCER');
 	}
