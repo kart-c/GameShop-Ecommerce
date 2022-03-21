0b5198236c9e0933f6421c7fade1ad87e22d728f
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-21 16:10:33 +0530

    feat: create add-to-cart handler and export it

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -3,3 +3,4 @@ export { checkInStock } from './check-in-stock';
 export { priceFilter } from './price-filter';
 export { productSort } from './product-sort';
 export { ratingFilter } from './rating-filter';
+export { addToCartHandler } from './add-to-cart';
