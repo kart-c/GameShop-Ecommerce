4f7ab44d1522d28c16f26ff30a386bdd79c05132
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-21 16:12:06 +0530

    feat: handle card button status

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCart } from '../../Context';
 import styles from './Card.module.css';
 
-const Card = ({ badge, categoryName, image, price, rating, title, wishlist }) => {
+const Card = ({
+	badge,
+	categoryName,
+	image,
+	price,
+	rating,
+	title,
+	wishlist,
+	cartBtnHandler,
+	_id,
+	checkCartStatus,
+}) => {
+	const navigate = useNavigate();
 	return (
 		<article className={`card product-card card-shadow ${styles.card}`}>
 			<img src={image} alt="card image 1" className={`card-img ${styles.cardImg}`} />
@@ -25,9 +38,20 @@ const Card = ({ badge, categoryName, image, price, rating, title, wishlist }) =>
 					</small>
 					<small className={styles.cardDiscount}> (60% OFF)</small>
 				</p>
-				<Link to="/cart" className={`action-link ${styles.actionLink}`}>
-					Add to Cart
-				</Link>
+				<button
+					disabled={badge === 'Out of Stock'}
+					className={`action-link ${styles.actionBtn}`}
+					title={badge === 'Out of Stock' ? 'Item is out of stock' : ''}
+					onClick={() => cartBtnHandler(_id)}
+				>
+					{checkCartStatus(_id) === 'Go to Cart' ? (
+						<span onClick={() => navigate('/cart')} className={styles.nav}>
+							Go to Cart
+						</span>
+					) : (
+						<span>Add to Cart</span>
+					)}
+				</button>
 			</div>
 		</article>
 	);
