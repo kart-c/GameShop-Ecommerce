a19e79b9cd58a2e7cac3e80b3d56418710837a09
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-22 16:27:25 +0530

    feat: calculate pricing based on discount

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import styles from './Card.module.css';
 
 const Card = ({
+	discount,
 	badge,
 	categoryName,
 	image,
 	price,
 	rating,
 	title,
-	wishlist,
 	cartBtnHandler,
 	_id,
 	checkCartStatus,
@@ -23,7 +23,7 @@ const Card = ({
 				<h4>{title}</h4>
 				<span>{categoryName} - PC game</span>
 				<div className="overlay-icon">
-					<i className={`far fa-heart ${wishlist && 'fas'}`}></i>
+					<i className="far fa-heart"></i>
 				</div>
 				{badge && (
 					<span className={`product-card-badge ${styles.productCardBadge}`}>
@@ -32,11 +32,14 @@ const Card = ({
 				)}
 				<div className={styles.rating}>Ratings: {rating}/5</div>
 				<p>
-					<strong>Rs. {price}</strong>{' '}
-					<small>
-						<s>Rs. 2999</s>
-					</small>
-					<small className={styles.cardDiscount}> (60% OFF)</small>
+					{discount ? (
+						<strong>{(price - (discount / 100) * price).toFixed(0)} </strong>
+					) : (
+						<strong>{price} </strong>
+					)}
+
+					<small>{discount && <s>Rs. {price}</s>}</small>
+					{discount && <small className={styles.cardDiscount}> ({discount}% OFF)</small>}
 				</p>
 				{checkCartStatus(_id) === 'Add to Cart' ? (
 					<button
