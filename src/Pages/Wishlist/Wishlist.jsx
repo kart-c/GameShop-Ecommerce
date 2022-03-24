8eaccdea7f65afa12fb8d6fccfa8af5dfb1340ff
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-24 12:27:38 +0530

    feat: add wishlist fetch function when user navigates to wishlist page

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card, Header } from '../../Components';
 import styles from './Wishlist.module.css';
+import axios from 'axios';
+import { useAuth, useWishlist } from '../../Context';
 
 const Wishlist = () => {
+	const { authState } = useAuth();
+
+	const {
+		wishlistState: { wishlist },
+		wishlistDispatch,
+	} = useWishlist();
+
+	const fetchWishlist = async () => {
+		try {
+			const response = await axios.get('/api/user/wishlist', {
+				headers: { authorization: authState.token },
+			});
+			if (response.status === 200) {
+				wishlistDispatch({ type: 'INITIAL', payload: response.data.wishlist });
+			} else {
+				console.error('ERROR: ', response);
+			}
+		} catch (error) {
+			console.log('ERROR: ', error);
+		}
+	};
+
+	useEffect(() => fetchWishlist(), []);
+
 	return (
 		<>
 			<Header />
@@ -10,9 +36,9 @@ const Wishlist = () => {
 				<h3 className={styles.wlHeading}>My Wishlist</h3>
 				<small>3 Items</small>
 				<section className={styles.productList}>
-					<Card />
-					<Card />
-					<Card />
+					{wishlist.map((wishlistItem) => (
+						<Card key={wishlistItem._id} {...wishlistItem} products={wishlist} />
+					))}
 				</section>
 			</main>
 		</>
