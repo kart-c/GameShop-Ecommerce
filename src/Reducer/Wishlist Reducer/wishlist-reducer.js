d432482cb82c04dd394a53431dc07d18022abd77
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-24 12:27:51 +0530

    feat: add wishlist reducer

--- src/Reducer/Wishlist Reducer/wishlist-reducer.js
+++ src/Reducer/Wishlist Reducer/wishlist-reducer.js
@@ -1,1 +1,18 @@
-export const wishlistReducer = (state, action) => {};
+export const wishlistReducer = (state, action) => {
+	const { payload } = action;
+	switch (action.type) {
+		case 'INITIAL':
+			return { ...state, wishlist: payload };
+
+		case 'ADD_TO_WISHLIST':
+			console.log({ ...state, wishlist: payload });
+			return { ...state, wishlist: payload };
+
+		case 'REMOVE_FROM_WISHLIST':
+			console.log({ ...state, wishlist: payload });
+			return { ...state, wishlist: payload };
+
+		default:
+			throw new Error('ERROR!!! NO CASE FOUND IN WISHLIST REDUCER');
+	}
+};
