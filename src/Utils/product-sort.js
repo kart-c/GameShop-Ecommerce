4488894dd2cfbcd0803be74922c19e3ab504c4f6
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-02 11:21:35 +0530

    fix: fix sort price on discounted price instead of actual price

--- src/Utils/product-sort.js
+++ src/Utils/product-sort.js
@@ -1,9 +1,21 @@
 export const productSort = (state, products) => {
+	const discoutedPrice = (price, discount = 0) => price - (discount / 100) * price;
+
 	if (state.sortBy === 'lowtohigh') {
-		return [...products].sort((product1, product2) => product1.price - product2.price);
+		return [...products].sort(
+			(product1, product2) =>
+				discoutedPrice(product1.price, product1.discount) -
+				discoutedPrice(product2.price, product2.discount)
+		);
 	}
+
 	if (state.sortBy === 'hightolow') {
-		return [...products].sort((product1, product2) => product2.price - product1.price);
+		return [...products].sort(
+			(product1, product2) =>
+				discoutedPrice(product2.price, product2.discount) -
+				discoutedPrice(product1.price, product1.discount)
+		);
 	}
+
 	return products;
 };
