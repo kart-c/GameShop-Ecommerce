fd6ab289d2cf91ea7644ca987058e84f92ba903a
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-02 17:11:42 +0530

    fix: fix typo

--- src/Utils/product-sort.js
+++ src/Utils/product-sort.js
@@ -1,19 +1,19 @@
 export const productSort = (state, products) => {
-	const discoutedPrice = (price, discount = 0) => price - (discount / 100) * price;
+	const discountedPrice = (price, discount = 0) => price - (discount / 100) * price;
 
 	if (state.sortBy === 'lowtohigh') {
 		return [...products].sort(
 			(product1, product2) =>
-				discoutedPrice(product1.price, product1.discount) -
-				discoutedPrice(product2.price, product2.discount)
+				discountedPrice(product1.price, product1.discount) -
+				discountedPrice(product2.price, product2.discount)
 		);
 	}
 
 	if (state.sortBy === 'hightolow') {
 		return [...products].sort(
 			(product1, product2) =>
-				discoutedPrice(product2.price, product2.discount) -
-				discoutedPrice(product1.price, product1.discount)
+				discountedPrice(product2.price, product2.discount) -
+				discountedPrice(product1.price, product1.discount)
 		);
 	}
 
