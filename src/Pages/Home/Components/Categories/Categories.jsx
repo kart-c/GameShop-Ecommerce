56403fa41a8b9c4442baefaee0ccbb98c4de4f40
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-05 11:47:13 +0530

    feat: filter from home
    filterdispatch added from home, to show clicked category already filtered in products page

--- src/Pages/Home/Components/Categories/Categories.jsx
+++ src/Pages/Home/Components/Categories/Categories.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { RDR2, Fifa, AOE4, GOW } from '../../../../Assets/images';
+import { useFilter } from '../../../../Context';
 import styles from './Categories.module.css';
 
 const Categories = () => {
+	const { filterDispatch } = useFilter();
+
 	return (
 		<div className={styles.categoryList}>
-			<Link to="/products" className={styles.categoryItem}>
+			<Link
+				to="/products"
+				className={styles.categoryItem}
+				onClick={() =>
+					filterDispatch({
+						type: 'CATEGORY_FILTER',
+						payload: { type: 'Action', isChecked: true },
+					})
+				}
+			>
 				<img src={RDR2} alt="Hero Image" />
 				<span>Action Games</span>
 			</Link>
-			<Link to="/products" className={styles.categoryItem}>
+			<Link
+				to="/products"
+				className={styles.categoryItem}
+				onClick={() =>
+					filterDispatch({
+						type: 'CATEGORY_FILTER',
+						payload: { type: 'Sports', isChecked: true },
+					})
+				}
+			>
 				<img src={Fifa} alt="Hero Image" />
 				<span>Sports Games</span>
 			</Link>
-			<Link to="/products" className={styles.categoryItem}>
+			<Link
+				to="/products"
+				className={styles.categoryItem}
+				onClick={() =>
+					filterDispatch({
+						type: 'CATEGORY_FILTER',
+						payload: { type: 'RPG', isChecked: true },
+					})
+				}
+			>
 				<img src={GOW} alt="Hero Image" />
 				<span>RPG Games</span>
 			</Link>
-			<Link to="/products" className={styles.categoryItem}>
+			<Link
+				to="/products"
+				className={styles.categoryItem}
+				onClick={() =>
+					filterDispatch({
+						type: 'CATEGORY_FILTER',
+						payload: { type: 'Strategy', isChecked: true },
+					})
+				}
+			>
 				<img src={AOE4} alt="Hero Image" />
 				<span>Strategy Games</span>
 			</Link>
