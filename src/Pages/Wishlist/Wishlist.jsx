d8aef0db2ea01bfc0dd23e21ea87469304a7d7e9
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-07 00:19:31 +0530

    feat: add loader on wishlist

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import { Card, Header } from '../../Components';
+import React, { useState, useEffect } from 'react';
+import { Card, Header, Loaders } from '../../Components';
 import styles from './Wishlist.module.css';
 import { useAuth, useWishlist } from '../../Context';
 import { Link } from 'react-router-dom';
 import { fetchWishlist } from '../../Utils';
 
 const Wishlist = () => {
+	const [isLoading, setIsLoading] = useState(true);
 	const { authState } = useAuth();
 
 	const {
@@ -13,13 +14,14 @@ const Wishlist = () => {
 		wishlistDispatch,
 	} = useWishlist();
 
-	useEffect(() => fetchWishlist(authState.token, wishlistDispatch), []);
+	useEffect(() => fetchWishlist(authState.token, wishlistDispatch, setIsLoading), []);
 
 	return (
 		<>
 			<Header />
+			{isLoading && <Loaders />}
 			<main className={styles.wlPg}>
-				{wishlist.length > 0 ? (
+				{wishlist.length > 0 && !isLoading ? (
 					<>
 						<h3 className={styles.wlHeading}>My Wishlist</h3>
 						<small>3 Items</small>
