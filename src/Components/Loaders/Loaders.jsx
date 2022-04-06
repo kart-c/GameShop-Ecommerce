aa6d36cfca3d8b4cd01206ebd38d350743524165
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-06 23:57:18 +0530

    refactor: refactor loader to stay in middle of screen

--- src/Components/Loaders/Loaders.jsx
+++ src/Components/Loaders/Loaders.jsx
@@ -1,7 +1,7 @@
 import styles from './Loaders.module.css';
 
 const Loaders = () => {
-	return <span className={styles.loader}>hello</span>;
+	return <span className={styles.loader}></span>;
 };
 
 export { Loaders };
