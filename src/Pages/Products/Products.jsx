5363b7c7402d5a94f0b0ba83fc7b29a724d7c66b
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-24 11:07:39 +0530

    feat: add pagination

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -21,8 +21,10 @@ const Products = () => {
 	const [isError, setIsError] = useState(false);
 	const [filterDisplay, setFilterDisplay] = useState(false);
 	const [searchValue, setSearchValue] = useState('');
+	const [currentPg, setCurrentPg] = useState(1);
+	const [pages, setPages] = useState([]);
+	const [currentSlice, setCurrentSlice] = useState({ start: 0, end: 6 });
 
-	// Fetch products from database
 	const fetchProducts = async () => {
 		try {
 			const response = await axios.get('/api/products');
@@ -56,6 +58,30 @@ const Products = () => {
 
 	const searchedProducts = searchProducts(sortedProducts, searchValue);
 
+	useEffect(() => {
+		setCurrentSlice((prev) => ({ ...prev, start: currentPg * 6 - 6, end: currentPg * 6 }));
+
+		window.scrollTo(0, 0);
+	}, [currentPg]);
+
+	const slicedProducts = searchedProducts.slice(currentSlice.start, currentSlice.end);
+
+	useEffect(() => {
+		const num = Math.ceil(searchedProducts.length / 6);
+		if (num) {
+			setPages([]);
+			for (let i = 0; i < num; i++) {
+				setPages((prev) => [...prev, i]);
+			}
+		}
+
+		if (slicedProducts.length < 6) {
+			setCurrentPg(1);
+		}
+	}, [searchedProducts.length]);
+
+	const paginationHandler = (page) => setCurrentPg(page + 1);
+
 	return (
 		<>
 			<div
@@ -73,18 +99,17 @@ const Products = () => {
 								Products
 							</Link>
 						</div>
-						{searchedProducts.length > 0 && (
+						{slicedProducts.length > 0 && (
 							<span>
-								Showing {searchedProducts.length}{' '}
-								{searchedProducts.length > 1 ? 'products' : 'product'}
+								Showing {slicedProducts.length} {slicedProducts.length > 1 ? 'products' : 'product'}
 							</span>
 						)}
 					</div>
 					{isLoading && <Loaders />}
 					{isError && <h2>Error ...</h2>}
 					{!isLoading && !isError ? (
-						searchedProducts.length > 0 ? (
-							searchedProducts.map((product) => (
+						slicedProducts.length > 0 ? (
+							slicedProducts.map((product) => (
 								<li key={product.id}>
 									<Card {...product} products={products} />
 								</li>
@@ -93,7 +118,21 @@ const Products = () => {
 							<h2>No Products found</h2>
 						)
 					) : null}
+					<div className={styles.pgButtonContainer}>
+						{pages.length > 0
+							? pages.map((page) => (
+									<button
+										key={page}
+										className={currentPg === page + 1 ? styles.pgActiveBtn : ''}
+										onClick={() => paginationHandler(page)}
+									>
+										{page + 1}
+									</button>
+							  ))
+							: null}
+					</div>
 				</ul>
+
 				<footer className={styles.filterFooter}>
 					<button className="btn-primary" onClick={() => setFilterDisplay(!filterDisplay)}>
 						Filters
